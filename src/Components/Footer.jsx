455632e2f892dee408d1d21d4d250a64504d1714
Lari <larihid1@gmail.com>
2024-02-28 12:56:07 -0600

    feat: responsive footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,17 +3,17 @@ import ImgFooter from '../assets/ffw.png'
 export const Footer = () => {
     return(
         <footer className="flex flex-col">
-            <div className="bg-[#79564A] w-full h-72">
-                <div className="flex flex-col ml-20 mt-14">
+            <div className="bg-[#79564A] w-full h-32 sm:h-72">
+                <div className="flex flex-col ml-5 mt-10 md:ml-20 md:mt-14">
                     <div className="flex items-center">
-                        <img src={ImgFooter} alt="ImgFooter" className='w-[130px] h-[130px]' />
-                        <h1 className="text-[#FFFFFF] font-thin text-[82px] ml-8">AKALI</h1>
+                        <img src={ImgFooter} alt="ImgFooter" className='w-[60px] sm:w-[130px] sm:h-[130px]' />
+                        <h1 className="text-[#FFFFFF] font-thin text-[40px] sm:text-[82px] ml-2 sm:ml-8">AKALI</h1>
                     </div>
-                    <div className='h-[1px] w-[1630px] bg-[#FFFFFF] mt-10'/>
+                    <div className='h-[1px] w-[1630px] bg-[#FFFFFF] mt-2 sm:mt-10'/>
                 </div>
             </div>
-            <div className="bg-[#FFFFFF] w-full h-20 flex items-center">
-                <h2 className='ml-32 text-[#848484] font-normal'>©2024 AKALI. Todos los derechos reservados</h2>
+            <div className="bg-[#FFFFFF] w-full h-14 sm:h-20 flex items-center">
+                <h2 className=' ml-5 sm:ml-32 text-[#848484] font-normal text-sm sm:text-base'>©2024 AKALI. Todos los derechos reservados</h2>
             </div>
         </footer>
     )
--- src/Components/Precauciones.jsx
+++ src/Components/Precauciones.jsx
@@ -9,12 +9,12 @@ export const Precauciones = () => {
         <div className="flex flex-col pt-14 pb-14 bg-[#FAFAFA]">
             <div className='flex flex-col items-end mr-20'>
                 <div className="flex flex-col items-end">
-                    <h2 className="text-[#603809] font-bold text-7xl">Precauciones</h2>
+                    <h2 className="text-[#603809] font-bold  text-4xl md:text-7xl">Precauciones</h2>
                     <div className="h-[4px] w-44 bg-[#603809] mt-5"/>
                 </div>
             </div>
             <div className='flex flex-row justify-center gap-48'>
-                <img src={ImgPrecauciones} alt="img-precauciones" className='object-contain' />
+                <img src={ImgPrecauciones} alt="img-precauciones" className='object-contain hidden sm:block' />
                 <div className="flex flex-col gap-10 mt-10">
                     <CardPrecauciones img={Card1} titulo='Mujeres embarazadas' descripcion='Se recomienda limitar la ingesta de cafeína durante el embarazo debido al riesgo de complicaciones.'/>
                     <CardPrecauciones img={Card2} titulo='Personas sensibles a la cafeína' descripcion='Algunas personas experimentan efectos secundarios como nerviosismo, insomnio y dolores de cabeza'/>
