ff1f861928a22603498e887d460f805e00c3ec37
Lari <larihid1@gmail.com>
2024-02-28 13:38:43 -0600

    merge

--- src/Components/CardPrecauciones.jsx
+++ src/Components/CardPrecauciones.jsx
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types'
 
 export const CardPrecauciones = ({img, titulo, descripcion}) => {
     return (
-        <div className='flex flex-row  gap-5 sm:gap-10 items-center'>
-            <div className='flex items-center justify-center shadow-2xl rounded-full border-4 border-[#633A2C] w-[60px] h-[60px] sm:w-[100px] sm:h-[100px]'>
-                <img src={img} alt="img-card-precauciones" className='w-[40px] h-[40px] sm:w-[70px] sm:h-[70px]' />
+        <div className='flex flex-row  gap-5 md:gap-10 items-center'>
+            <div className='flex items-center justify-center shadow-2xl rounded-full border-4 border-[#633A2C] w-[60px] h-[60px] md:w-[100px] md:h-[100px]'>
+                <img src={img} alt="img-card-precauciones" className='w-[40px] h-[40px] md:w-[70px] md:h-[70px]' />
             </div>
             <div className='flex flex-col gap-5'>
-                <h2 className='text-[#603809] font-semibold text-lg sm:text-2xl'>{titulo}</h2>
-                <p className='text-[#5A5A5A] font-light w-[270px] sm:w-[473px] text-base sm:text-xl'>{descripcion}</p>
+                <h2 className='text-[#603809] font-semibold text-lg md:text-2xl'>{titulo}</h2>
+                <p className='text-[#5A5A5A] font-light w-[270px] md:w-[473px] text-base md:text-xl'>{descripcion}</p>
             </div>
         </div>
     )
--- src/Components/Inicio.jsx
+++ src/Components/Inicio.jsx
@@ -3,8 +3,8 @@ import ImgInicio from '../assets/TazaInicio.png';
 export const Inicio = () => {
     return (
         <div className='flex flex-col pb-20 pt-4 sm:pt-10'>
-            <div className="flex flex-col md:flex-row justify-around  md:mt-16">
-                <div className="flex flex-col items-center md:items-start p-10 sm:p-0">
+            <div className="flex flex-col md:flex-row justify-around  lg:mt-16">
+                <div className="flex flex-col items-center lg:items-start p-10 md:p-0">
                     <h2 className="text-[#603809] text-center font-bold text-4xl md:text-7xl">¿QUÉ ES EL CAFÉ?</h2>
                     <div className='h-[4px] w-full md:w-44 bg-[#603809] mt-5'/>
                     <p className="text-justify leading-8 md:leading-9 text-[#5A5A5A] font-light text-lg md:text-2xl mt-5 md:ml-10">
